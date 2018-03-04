fe1a4ebe648c56d7bab682059896313eb924399f
gable44 <gable4444@gmail.com>
2018-03-05 00:05:41 +0100

    Create hu-HU.php
    This is  the Hungarian translation for the project! :)

---
+++ l10n/hu-HU.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * i-MSCP LetsEncrypt plugin
+ * Copyright (C) 2017 Cambell Prince <cambell.prince@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * @translator  Gable <gable@gable4you.com>
+ */
+
+return array(
+    'LetsEncrypt support scheduled for activation. This can take few seconds.' => 'LetsEncrypt támogatásának aktiválása ütemezve van. Ez eltarthat néhány másodpercig.',
+    'LetsEncrypt support scheduled for deactivation. This can take few seconds.' => 'LetsEncrypt támogatásának deaktiválása ütemezve van. Ez eltarthat néhány másodpercig.',
+    'Generation in progress.' => 'A létrehozás folyamatban van.',
+    'n/a' => 'n/a',
+    'LetsEncrypt entries for customer: %s' => 'LetsEncrypt bejegyzései a következő ügyél részére: %s',
+    'Deactivate LetsEncrypt' => 'LetsEncrypt deaktiválása',
+    'No customer with LetsEncrypt support has been found.' => 'Nem található ügyfél LetsEncrypt támogatással.',
+    'Customers / LetsEncrypt' => 'Ügyfelek / LetsEncrypt',
+    'Select a customer' => 'Válasszon ügyfelet',
+    'Activate LetsEncrypt for this customer' => 'LetsEncrypt aktiválása ennél az ügyfélnél',
+    'Domain Name' => 'Domain név',
+    'LetsEncrypt domain key' => 'LetsEncrypt domain kulcs',
+    'Status' => 'Állapot',
+    'Are you sure you want to deactivate LetsEncrypt for this customer?' => 'Biztosan deaktiválni szeretnéd a LetsEnctyptet ennél a felhasználónál?',
+    'No domain with LetsEncrypt support has been found.' => 'Nem található domain LetsEncrypt támogatással.',
+    'Domain' => 'Domain',
+    'Name' => 'Név',
+    'LetsEncrypt' => 'LetsEncrypt'
+);
